c6fe3d725063cd3732e91839d2f92175ebdadcb4
Hector <hec@ungravity.net>
2015-11-15 16:54:30 +1100

    fix bin execution

--- vagrant-builds/generate-ca.sh
+++ vagrant-builds/generate-ca.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build cfssl binaries and copy outputs to the ssl folder
-./mnt/build-outputs/cfssl_linux-amd64 genkey -initca -config ca-config.json ca-csr.json \
-    |  ./mnt/build-outputs/cfssljson_linux-amd64 -bare cluster-ca \
+/mnt/build-outputs/cfssl_linux-amd64 genkey -initca -config ca-config.json ca-csr.json \
+    |  /mnt/build-outputs/cfssljson_linux-amd64 -bare cluster-ca \
      && mv cluster-ca-key.pem  cluster-ca.csr  cluster-ca.pem /mnt/ssl-outputs/
 
