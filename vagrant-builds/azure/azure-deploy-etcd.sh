a7e86f9b66e3b746e64e27e13439aa0f4da6b4c4
Hector <hec@ungravity.net>
2015-11-23 00:59:48 +1100

    windows eol fix

--- vagrant-builds/azure/azure-deploy-etcd.sh
+++ vagrant-builds/azure/azure-deploy-etcd.sh
@@ -2,8 +2,8 @@
 # http://go.microsoft.com/fwlink/?LinkId=254432
 
 cd /vagrant/azure && docker run -ti --rm \
--v $(ls | grep -i *publishsettings | sed -e "s|^|`pwd`/|g"):/credentials \
+-v $(ls | grep -i *publishsettings | sed -e "s|^|`pwd`/|g"):/tmp/credentials \
 -v /mnt/cluster-files:/cluster-files \
 -v /mnt/bootstrap-files:/bootstrap-files \
 -v /vagrant/azure/scripts:/tmp/deploy \
-microsoft/azure-cli /bin/bash -c "azure account import credentials;./tmp/deploy/etcd-infra.sh;./tmp/deploy/etcd-peers.sh"
+microsoft/azure-cli /bin/bash -c "cat /tmp/credentials | tr -d '\015' > credentials; azure account import credentials;./tmp/deploy/etcd-infra.sh;./tmp/deploy/etcd-peers.sh"
--- vagrant-builds/azure/azure-destroy-etcd-storage.sh
+++ vagrant-builds/azure/azure-destroy-etcd-storage.sh
@@ -2,9 +2,9 @@
 # http://go.microsoft.com/fwlink/?LinkId=254432
 
 cd /vagrant/azure && docker run -ti --rm \
--v $(ls | grep -i *publishsettings | sed -e "s|^|`pwd`/|g"):/credentials \
+-v $(ls | grep -i *publishsettings | sed -e "s|^|`pwd`/|g"):/tmp/credentials \
 -v /mnt/cluster-files:/cluster-files \
 -v /mnt/bootstrap-files:/bootstrap-files \
 -v /vagrant/azure/scripts:/tmp/deploy \
-microsoft/azure-cli /bin/bash -c "azure account import credentials;./tmp/deploy/etcd-destroy-storage.sh"
+microsoft/azure-cli /bin/bash -c "cat /tmp/credentials | tr -d '\015' > credentials; azure account import credentials;./tmp/deploy/etcd-destroy-storage.sh"
 
